3c6c6073764e0690000be4b3d81f8e5da6ea2fea
Adam Butler <adam@lab.io>
2015-04-19 11:12:37 +0100

    Exit when channel presence is over

--- app/workers/twitter_worker.rb
+++ app/workers/twitter_worker.rb
@@ -6,13 +6,10 @@ class TwitterWorker
     TWITTER_CLIENT.filter(:track => query) do |object|
       if object.is_a?(Twitter::Tweet)
         # To Do: Will use pusher to send to the client
-        tweets << object.text
         puts object.text
       end
 
-      # To Do: presence channel to check if the user is still
-      # on the page.
-      if tweets.size > 5
+      unless Pusher["presence-#{query}"].info[:occupied]
         return
       end
     end
